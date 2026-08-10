645e9612f68cc3731c748acc783f9f4d07f9efb6
MUSTAFA SHAIK <106308110+devops0014@users.noreply.github.com>
2026-08-11 00:22:10 +0530

    Update docker-compose.sh

--- docker-compose.sh
+++ docker-compose.sh
@@ -3,3 +3,14 @@ sudo curl -SL "https://github.com/docker/compose/releases/latest/download/docker
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 docker-compose version
+
+
+#buildx commands
+# Get the tag name of the latest release
+BUILDX_VERSION=$(curl -s https://api.github.com/repos/docker/buildx/releases/latest | grep -oP '"tag_name": "\K[^"]+')
+# Detect architecture and download
+ARCH=$(uname -m)
+if [ "$ARCH" = "x86_64" ]; then ARCH="amd64"; elif [ "$ARCH" = "aarch64" ]; then ARCH="arm64"; fi
+curl -Lo ~/.docker/cli-plugins/docker-buildx "https://github.com/docker/buildx/releases/download/${BUILDX_VERSION}/buildx-${BUILDX_VERSION}.linux-${ARCH}"
+chmod +x ~/.docker/cli-plugins/docker-buildx
+docker buildx version
